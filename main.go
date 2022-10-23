f58e012cf07bab68f4269592acdcf02f46936fee
Ivan Mamtsev <ivan.mamtsev@hexlet.io>
2022-10-23 19:28:11 +0400

    add games to main script

--- main.go
+++ main.go
@@ -5,14 +5,16 @@ import (
 	braingames "hexletcode/brain-games"
 	calcgame "hexletcode/brain-games/calc_game"
 	evengame "hexletcode/brain-games/even_game"
+	gcdgame "hexletcode/brain-games/gcd_game"
 )
 
 func main() {
-	fmt.Println("Please enter the game number.")
+	fmt.Println("Please enter the game number and press Enter.")
 	fmt.Println("0 - Exit")
 	fmt.Println("1 - Greet")
-	fmt.Println("2 - EvenGame")
-	fmt.Println("3 - CalcGame")
+	fmt.Println("2 - Even")
+	fmt.Println("3 - Calc")
+	fmt.Println("4 - GCD")
 	var choice string
 	fmt.Scanln(&choice)
 	fmt.Printf("Your choice: %s\n", choice)
@@ -25,5 +27,7 @@ func main() {
 		evengame.Run()
 	case "3":
 		calcgame.Run()
+	case "4":
+		gcdgame.Run()
 	}
 }
